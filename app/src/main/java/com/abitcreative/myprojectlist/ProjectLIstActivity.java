8178d1223da79ad336b63e9dc7a348ee81da3a89
Michal Dabrowski <michal.dabrowski@neofonie.de>
2016-09-10 16:19:08 +0200

    Initial Commit

---
+++ app/src/main/java/com/abitcreative/myprojectlist/ProjectLIstActivity.java
@@ -0,0 +1,13 @@
+package com.abitcreative.myprojectlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectLIstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_list);
+    }
+}
